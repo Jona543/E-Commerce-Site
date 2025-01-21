2f6c6a20d4d7f071747f048eee4858ceebf81b16
Jonathan Tromp <jonatromp45@gmail.com>
2025-01-21 12:46:34 +0000

    Added links on navbar for different categories, changes navbar width styling, defined different pages

--- E-Commerce_frontend/src/App.jsx
+++ E-Commerce_frontend/src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route, } from 'react-router-dom'
+import ShopCategory from './Pages/ShopCategory'
+import Product from './Pages/Product'
+import LoginSignup from './Pages/LoginSignup'
+import Shop from './Pages/Shop'
+import Cart from './Pages/Cart'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/men' element={<ShopCategory category="men"/>}/>
+        <Route path='/women' element={<ShopCategory categroy="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path='/productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
