9f11ed5777649f579f7a4e8ee3bbc5cfa997609f
Jonathan Tromp <jonatromp45@gmail.com>
2025-01-22 20:55:42 +0000

    completed home page, added seperate styling sheets for each page

--- E-Commerce_frontend/src/App.jsx
+++ E-Commerce_frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Product from './Pages/Product'
 import LoginSignup from './Pages/LoginSignup'
 import Shop from './Pages/Shop'
 import Cart from './Pages/Cart'
+import { Footer } from './Components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,6 +27,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   )
