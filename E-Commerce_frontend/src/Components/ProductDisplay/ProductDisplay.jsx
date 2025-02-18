5d97b904d7b90aa2f2da20206986d5e4271195ea
Jonathan Tromp <jonatromp45@gmail.com>
2025-02-18 22:36:52 +0000

    finished all context referencing across all pages, added information for product from source file. All styling completed as well as navigation and links

--- E-Commerce_frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ E-Commerce_frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import './ProductDisplay.css'
 import star_icon from '../assets/star_icon.png';
 import star_dull_icon from '../assets/star_dull_icon.png';
+import { ShopContext } from '../../Context/ShopContext';
+import { useContext } from 'react';
 
 export const ProductDisplay = (props) => {
     const {product} = props
+    const {addToCart} = useContext(ShopContext);
   return (
     <div className='productdisplay'>
         <div className="productdisplay-left">
@@ -20,7 +23,7 @@ export const ProductDisplay = (props) => {
         </div>
         <div className="productdisplay-right">
             <h1>{product.name}</h1>
-            <div className="productdisplay-right-star">
+            <div className="productdisplay-right-stars">
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
@@ -35,6 +38,19 @@ export const ProductDisplay = (props) => {
             <div className="productdisplay-right-description">
                 A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.
             </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-size">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button onClick={()=>{addToCart(product.id)}}>Add To Cart</button>
+            <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop Top</p>
+            <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
         </div>
     </div>
   )
--- E-Commerce_frontend/src/Pages/Product.jsx
+++ E-Commerce_frontend/src/Pages/Product.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
-import { Breadcrumb } from 'antd'
+import { Breadcrumb } from '../Components/Breadcrumbs/Breadcrumb'
 import { ProductDisplay } from '../Components/ProductDisplay/ProductDisplay'
+import { DescriptionBox } from '../Components/DescriptionBox/DescriptionBox'
+import { RelatedProducts } from '../Components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
@@ -13,6 +15,8 @@ const Product = () => {
     <div>
       <Breadcrumb product={product}/>
       <ProductDisplay product={product}/>
+      <DescriptionBox/>
+      <RelatedProducts/>
     </div>
   )
 }
